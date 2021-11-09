fe5d295cb781c99ff6fde8f9be0fe17efacf9a37
Yenny Maldonado <yenyamaldonado@gmail.com>
2021-11-09 10:16:55 -0600

    prueba para pull request

--- src/componentes/Footer.js
+++ src/componentes/Footer.js
@@ -1,7 +1,8 @@
 const Footer = () => {
-    const legal = document.createElement('div');
-    legal.className = 'legalArea';
-    legal.textContent = 'Todos los derechos son reservados';
-    return legal;
-}
+  const legal = document.createElement('div');
+  legal.className = 'legalArea';
+  legal.textContent = 'Todos los derechos son reservados';
+  return legal;
+};
+// probando cambios para pull
 export default Footer;
